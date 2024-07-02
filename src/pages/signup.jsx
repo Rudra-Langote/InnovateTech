241d4ae00bf4ae2f951ca3f39b3f9ee1f1166a8a
Rudra-Lanote <langoterudra@gmail.com>
2024-07-03 00:07:51 +0530

    Edited

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -23,116 +23,120 @@ const Signup = ({ users }) => {
   const passref = useRef(null);
   const pass2ref = useRef(null);
   const addressref = useRef(null);
+  const dyseref = useRef(null);
   const user = useRef([{}]);
-  
-  
-    async function get({ users, router }) {
-
-      document.getElementById("dyserr").classList.add("hidden");
 
 
- 
-  
-      
-
-      user.current = {
-        firstname: fnameref.current.value,
-        lastname: lnameref.current.value,
-        email: emailref.current.value,
-        username: usernameref.current.value,
-        pass: pass2ref.current.value,
-        address: addressref.current.value
-      }
-
-      console.log(user.current.pass)
-      console.log(pass2ref.current.value)
-      
+  async function get({ users, router }) {
 
 
-      if (user.current.firstname == "") {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('First name is required')
-      } else if (user.current.lastname == "") {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('Last name is required')
-      }
-      else if (user.current.email == "") {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('Email is required')
-      }
-      else if (user.current.username == "") {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('Username is required')
-      }
+    dyseref.current.classList.add("hidden")
 
-      else if (user.current.address == "") {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('Address is required')
-      }
 
-      else if (passref.current.value == "") {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('Password is required')
 
-      } else if (user.current.pass == "") {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('Please re-enter the password')
-      }
-      else if (passref.current.value != user.current.pass) {
-        document.getElementById("dyserr").classList.toggle("hidden");
-        setError('You re-enterd wrong password')
-      }
-      else {
-        if (users.includes(user.current.username)) {
-          document.getElementById("dyserr").classList.toggle("hidden");
-          setError('This username already exiest')
-        }
-        else {
-          isLoading(true)
+
+
+    user.current = {
+      firstname: fnameref.current.value,
+      lastname: lnameref.current.value,
+      email: emailref.current.value,
+      username: usernameref.current.value,
+      pass: pass2ref.current.value,
+      address: addressref.current.value
+    }
     
 
 
+    if (user.current.firstname == "") {
+      dyseref.current.classList.toggle("hidden")
+      fnameref.current.focus();
+      setError('First name is required')
+    } else if (user.current.lastname == "") {
+      dyseref.current.classList.toggle("hidden")
+      lnameref.current.focus();
+      setError('Last name is required')
+    }
+    else if (user.current.email == "") {
+      dyseref.current.classList.toggle("hidden")
+      emailref.current.focus();
+      setError('Email is required')
+    }
+    else if (user.current.username == "") {
+      dyseref.current.classList.toggle("hidden")
+      usernameref.current.focus();
+      setError('Username is required')
+    }
 
 
-          document.getElementById("dyserr").classList.add("hidden");
 
-          var j = [user.current]
-          const res = await fetch(`${API_URL}/api/adduser`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(j)
+    else if (passref.current.value == "") {
+      dyseref.current.classList.toggle("hidden")
+      passref.current.focus();
+      setError('Password is required')
+
+    } else if (user.current.pass == "") {
+      dyseref.current.classList.toggle("hidden")
+      pass2ref.current.focus();
+      setError('Please re-enter the password')
+    }
+    else if (passref.current.value != user.current.pass) {
+      dyseref.current.classList.toggle("hidden")
+      pass2ref.current.focus();
+      setError('You re-enterd wrong password')
+    }
+    else if (user.current.address == "") {
+      dyseref.current.classList.toggle("hidden")
+      addressref.current.focus();
+      setError('Address is required')
+    }
+    else {
+      if (users.includes(user.current.username)) {
+        dyseref.current.classList.toggle("hidden")
+        setError('This username already exiest')
+      }
+      else {
+        isLoading(true)
+
+        dyseref.current.classList.add("hidden")
+
+        var j = [user.current]
+        const res = await fetch(`${API_URL}/api/adduser`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(j)
 
 
-          })
-          isLoading(false)
-          const data = true
-          setSharedValues({ value1: data, value2: user.current.username })
-          router.push({ pathname: '/' })
-
-        }
-
+        })
+        isLoading(false)
+        const data = true
+        setSharedValues({ value1: data, value2: user.current.username })
+        router.push({ pathname: '/' })
 
       }
 
-  
-
 
     }
-    useEffect(()=>{
-      Loading ?  setSup(<div class="flex p-2 gap-2">
-        <div class="w-2 h-2 rounded-full animate-pulse bg-white"></div>
-        <div class="w-2 h-2 rounded-full animate-pulse bg-white"></div>
-        <div class="w-2 h-2 rounded-full animate-pulse bg-white"></div>
+
+
+
+
+  }
+
+
+
+  useEffect(() => {
+    Loading ? setSup(<div class="flex p-2 gap-2">
+      <div class="w-2 h-2 rounded-full animate-pulse bg-white"></div>
+      <div class="w-2 h-2 rounded-full animate-pulse bg-white"></div>
+      <div class="w-2 h-2 rounded-full animate-pulse bg-white"></div>
     </div>) : setSup('Sign up');
-    })
-  
-  
-  // function lod() {
-  //   Loading ?  setSup(<svg aria-hidden="true" class="w-8 h-8 mx-auto my-auto text-gray-200 animate-spin dark:text-white fill-black" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>) : setSup('Sign up');
-  // }
-  
+  })
+
+
+
+
 
 
   return (
@@ -148,8 +152,8 @@ const Signup = ({ users }) => {
 
             <span className=" text-3xl mt-10  bg-black text-white pl-1">Sign<span
               className="bg-white text-3xl text-black font-bold">up</span></span>
-            <div style={{height:'70px'}}>
-              <div id="dyserr" className="m-2 hidden opacity-70" role="alert">
+            <div style={{ height: '70px' }}>
+              <div ref={dyseref} className="m-2 hidden opacity-70" role="alert">
                 <div className="bg-red-500 text-white font-bold rounded-t px-4 py-1">
                   Wrong Input
                 </div>
@@ -166,7 +170,7 @@ const Signup = ({ users }) => {
 
 
             <input ref={emailref} type="email" placeholder="Email" className=" outline-none  rounded-sm mt-5 border-b-2 border-black" />
-            
+
 
             <input ref={usernameref} type="text" placeholder="Enter Username" className="  outline-none  rounded-sm mt-5 border-b-2 border-black" />
 
