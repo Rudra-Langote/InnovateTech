fd1f6d01468d9c520a38c65a3971d0d4eb993069
Rudra-Lanote <langoterudra@gmail.com>
2024-07-02 23:03:02 +0530

    Edited

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router'
 import { useContext } from "react"
 import AppContext from "../context/AppContext"
 import Head from "next/head"
+import { useRef } from "react"
 
 
 const Signup = ({ users }) => {
@@ -15,79 +16,87 @@ const Signup = ({ users }) => {
   const [Error, setError] = useState("");
   const [Loading, isLoading] = useState(false);
   const [Sup, setSup] = useState('Sign up');
+  const fnameref = useRef(null);
+  const lnameref = useRef(null);
+  const emailref = useRef(null);
+  const usernameref = useRef(null);
+  const passref = useRef(null);
+  const pass2ref = useRef(null);
+  const addressref = useRef(null);
+  const user = useRef([{}]);
+  
   
     async function get({ users, router }) {
-      // lod()
 
       document.getElementById("dyserr").classList.add("hidden");
 
 
-      let firstName = document.getElementById("fname").value
-      let lastName = document.getElementById("lname").value
-      let Email = document.getElementById("email").value
-      let Password = document.getElementById("pass").value
-      let Password2 = document.getElementById("pass2").value
-      let Address = document.getElementById("address").value
-      let Username = document.getElementById("username").value
+ 
+  
+      
+
+      user.current = {
+        firstname: fnameref.current.value,
+        lastname: lnameref.current.value,
+        email: emailref.current.value,
+        username: usernameref.current.value,
+        pass: pass2ref.current.value,
+        address: addressref.current.value
+      }
+
+      console.log(user.current.pass)
+      console.log(pass2ref.current.value)
+      
 
 
-      if (firstName == "") {
+      if (user.current.firstname == "") {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('First name is required')
-      } else if (lastName == "") {
+      } else if (user.current.lastname == "") {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('Last name is required')
       }
-      else if (Email == "") {
+      else if (user.current.email == "") {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('Email is required')
       }
-      else if (Username == "") {
+      else if (user.current.username == "") {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('Username is required')
       }
 
-      else if (Address == "") {
+      else if (user.current.address == "") {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('Address is required')
       }
 
-      else if (Password == "") {
+      else if (passref.current.value == "") {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('Password is required')
 
-      } else if (Password2 == "") {
+      } else if (user.current.pass == "") {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('Please re-enter the password')
       }
-      else if (Password != Password2) {
+      else if (passref.current.value != user.current.pass) {
         document.getElementById("dyserr").classList.toggle("hidden");
         setError('You re-enterd wrong password')
       }
       else {
-        if (users.includes(Username)) {
+        if (users.includes(user.current.username)) {
           document.getElementById("dyserr").classList.toggle("hidden");
           setError('This username already exiest')
         }
         else {
           isLoading(true)
+    
 
 
 
 
           document.getElementById("dyserr").classList.add("hidden");
 
-
-
-          var j = [{
-            "firstname": firstName,
-            "lastname": lastName,
-            "email": Email,
-            "username": Username,
-            "pass": Password2,
-            "address": Address
-          }]
-
+          var j = [user.current]
           const res = await fetch(`${API_URL}/api/adduser`, {
             method: 'POST',
             headers: {
@@ -99,7 +108,7 @@ const Signup = ({ users }) => {
           })
           isLoading(false)
           const data = true
-          setSharedValues({ value1: data, value2: Username })
+          setSharedValues({ value1: data, value2: user.current.username })
           router.push({ pathname: '/' })
 
         }
@@ -150,23 +159,23 @@ const Signup = ({ users }) => {
               </div>
             </div>
 
-            <input id="fname" type="text" placeholder="First Name" className=" outline-none   rounded-sm border-b-2  mt-5 border-black" />
+            <input ref={fnameref} type="text" placeholder="First Name" className=" outline-none   rounded-sm border-b-2  mt-5 border-black" />
 
 
-            <input id="lname" type="text" placeholder="Lastname" className=" outline-none rounded-sm mt-5 border-b-2 border-black" />
+            <input ref={lnameref} type="text" placeholder="Lastname" className=" outline-none rounded-sm mt-5 border-b-2 border-black" />
 
 
-            <input id="email" type="email" placeholder="Email" className=" outline-none  rounded-sm mt-5 border-b-2 border-black" />
-            <label id="emailwrn" className=" text-sm absolute hidden top-60 text-red-700 left-24">*email required</label>
+            <input ref={emailref} type="email" placeholder="Email" className=" outline-none  rounded-sm mt-5 border-b-2 border-black" />
+            
 
-            <input id="username" type="text" placeholder="Enter Username" className="  outline-none  rounded-sm mt-5 border-b-2 border-black" />
+            <input ref={usernameref} type="text" placeholder="Enter Username" className="  outline-none  rounded-sm mt-5 border-b-2 border-black" />
 
 
-            <input id="pass" type="password" placeholder="Password" className=" outline-none  rounded-sm mt-5 border-b-2 border-black" />
+            <input ref={passref} type="password" placeholder="Password" className=" outline-none  rounded-sm mt-5 border-b-2 border-black" />
 
-            <input id="pass2" type="password" placeholder="Re-enter Password" className=" outline-none  rounded-sm mt-5 border-b-2 border-black" />
+            <input ref={pass2ref} type="password" placeholder="Re-enter Password" className=" outline-none  rounded-sm mt-5 border-b-2 border-black" />
 
-            <input id="address" type="text" placeholder="Address" className="  outline-none  rounded-sm mt-5 border-b-2 border-black" />
+            <input ref={addressref} type="text" placeholder="Address" className="  outline-none  rounded-sm mt-5 border-b-2 border-black" />
 
 
             <button onClick={() => get({ users, router })} className="bg-black p-1 hover:scale-110 duration-200 text-white mb-2 text-md rounded-xl mt-10  px-2">{Sup}</button>
